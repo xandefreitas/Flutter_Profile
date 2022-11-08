8a43bf9342325f1efe00d1d3e0f48c54aa2aef4b
Alexandre Freitas <xande_liborio@hotmail.com>
2022-11-08 10:03:24 -0300

    request notification permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,10 +8,19 @@ import 'package:flutter_profile/flutter_profile.dart';
 
 import 'common/bloc/languageBloc/language_bloc.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(_) async => await Firebase.initializeApp();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Locale locale = await SharedPreferencesUtil.getLocale();
   await Firebase.initializeApp();
+  FirebaseMessaging.instance.requestPermission();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
