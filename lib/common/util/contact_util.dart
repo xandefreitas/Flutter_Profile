137477a58731723a6f26596df354ff79da345459
Alexandre Freitas <xande_liborio@hotmail.com>
2023-02-24 17:16:37 +0100

    refactor: Launch Url

--- lib/common/util/contact_util.dart
+++ lib/common/util/contact_util.dart
@@ -2,18 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-abstract class ContactUtil {
-  static launchUrl(String url, BuildContext context) async {
-    final text = AppLocalizations.of(context)!;
+class ContactUtil {
+  final BuildContext context;
+  final AppLocalizations text;
+
+  ContactUtil({required this.context, required this.text});
+
+  launchUrl(String url) async {
     try {
       if (await canLaunchUrlString(url)) {
         await launchUrlString(url, mode: LaunchMode.externalApplication);
-      } else {
-        _showErrorOnUrlFailed(context, text.couldNotOpenUrlMessage);
       }
     } catch (e) {
       _showErrorOnUrlFailed(context, text.errorOpenningUrlMessage);
     }
+  }
+
+  launchWhatsApp(String url) {
+    final phoneNumber = url.replaceAll(' ', '');
+    final whatsAppUrl = 'https://wa.me/$phoneNumber?text=${Uri.encodeFull(text.urlMessage)}';
+    launchUrl(whatsAppUrl);
+  }
+
+  launchMail(String mail) {
+    final mailUrl = 'mailto:$mail?subject=${Uri.encodeFull('ProfileApp')}&body=${Uri.encodeFull(text.urlMessage)}';
+    launchUrl(mailUrl);
+  }
+
+  launchPhone(String cellphone) {
+    final phoneNumber = cellphone.replaceAll(' ', '');
+    final phoneUrl = 'tel:$phoneNumber';
+    launchUrl(phoneUrl);
   }
 
   static void _showErrorOnUrlFailed(BuildContext context, String message) {
