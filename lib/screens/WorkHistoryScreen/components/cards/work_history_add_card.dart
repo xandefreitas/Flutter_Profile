5f92a9bc153d8531ccc76ff17886fc8acd51541b
Alexandre <alexandrefreitas.dev@gmail.com>
2026-08-29 22:18:48 +0200

    style: WorkHistoryAddCard style

--- lib/screens/WorkHistoryScreen/components/cards/work_history_add_card.dart
+++ lib/screens/WorkHistoryScreen/components/cards/work_history_add_card.dart
@@ -12,59 +12,27 @@ class WorkHistoryAddCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                const Icon(
-                  Icons.home_work_outlined,
-                  color: AppColors.white,
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  AppLocalizations.of(context)!.workHistoryFormTitleAdd,
-                  style: AppTextStyles.textSize16.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.white,
-                  ),
-                ),
-              ],
-            ),
-            const Divider(
-              thickness: 1.5,
-              color: AppColors.white,
-            ),
-            InkWell(
-              onTap: () => Navigator.pushNamed(
-                context,
-                workHistoryFormRoute,
-                arguments: {
-                  'title': AppLocalizations.of(context)!.workHistoryFormTitleAdd,
-                  'addCompany': addWorkHistory,
-                  'screenMode': WorkHistoryScreenMode.ADD.value,
-                },
-              ),
-              child: Container(
-                height: 256,
-                width: double.infinity,
-                padding: const EdgeInsets.only(left: 8),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: AppColors.white.withValues(alpha: 0.5),
-                ),
-                child: const Icon(
-                  Icons.add,
-                  size: 40,
-                  color: AppColors.white,
-                ),
-              ),
-            ),
-          ],
+    return InkWell(
+      onTap:
+          () => Navigator.pushNamed(
+            context,
+            workHistoryFormRoute,
+            arguments: {
+              'title': AppLocalizations.of(context)!.workHistoryFormTitleAdd,
+              'addCompany': addWorkHistory,
+              'screenMode': WorkHistoryScreenMode.ADD.value,
+            },
+          ),
+      child: Container(
+        height: 104,
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+        padding: const EdgeInsets.only(left: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.white.withValues(alpha: 0.5),
         ),
+        child: const Icon(Icons.add, size: 40, color: AppColors.white),
       ),
     );
   }
